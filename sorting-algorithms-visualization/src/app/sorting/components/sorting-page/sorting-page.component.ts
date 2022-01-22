81aad7a02dca8c68f5777bb0ab5e1a51cc6d5251
Pawel Kubiak <pawel.kubiak@olympus-ossa.com>
2022-01-22 23:30:12 +0100

    Implement layout

--- sorting-algorithms-visualization/src/app/sorting/components/sorting-page/sorting-page.component.ts
+++ sorting-algorithms-visualization/src/app/sorting/components/sorting-page/sorting-page.component.ts
@@ -1,3 +1,5 @@
+import { ArrayHelper } from './../../../core/helpers/array.helper';
+import { SortService } from './../../services/sort.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sorting-page.component.scss']
 })
 export class SortingPageComponent implements OnInit {
+  public defaultImageSlicesUrls: string[];
+  public mixedUpImageSlicesUrls: string[];
 
-  constructor() { }
+  constructor(private sortService: SortService) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    // TODO: SOLID
+    this.setUpDefaultImageSlicesUrls();
+    this.mixedUpImageSlicesUrls = ArrayHelper.shuffle(ArrayHelper.clone(this.defaultImageSlicesUrls));
   }
 
+  private setUpDefaultImageSlicesUrls(): void {
+    this.defaultImageSlicesUrls = [];
+    for (let row = 1; row <= SortService.ROW_COUNT; row++) {
+      for (let column = 1; column <= SortService.COLUMN_COUNT; column++) {
+        this.defaultImageSlicesUrls.push(`../../../../assets/images/row-${row}-column-${column}.webp`);
+      }
+    }
+  }
 }
